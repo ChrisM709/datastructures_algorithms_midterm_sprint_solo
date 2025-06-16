3e137e8d22239a36cc02333f78540beb629716bf
ChrisM709 <christopher.meadus@keyin.com>
2025-06-16 18:00:16 -0300

    Added User.java - created the User class next because I needed a way to tie a username to its TaskList - defined a constructor that takes the user’s name and initializes a new TaskList. - added getName() to retrieve the user’s name - added addTask(String description) to create and add a new Task - added markTaskCompleted(int index) to mark a specific task as completed - added printTasks() to display all current tasks for that user

--- src/main/java/com/todolistmanager/User.java
+++ src/main/java/com/todolistmanager/User.java
@@ -1,5 +1,30 @@
 package com.todolistmanager;
 
 public class User {
-    
+    private String name;
+    private TaskList tasks;
+
+    public User(String name) {
+        this.name = name;
+        this.tasks = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTask(String description) {
+        tasks.addTask(new Task(description));
+        System.out.println("Added task for " + name + ": " + description);
+    }
+
+    public void markTaskCompleted(int index) {
+        System.out.println("Marking task " + index + " for " + name);
+        tasks.markCompleted(index);
+    }
+
+    public void printTasks() {
+        System.out.println("\nCurrent tasks for " + name + ":");
+        tasks.printTasks();
+    }
 }
