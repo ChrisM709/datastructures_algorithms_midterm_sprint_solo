4a0a4213971fe061e7fade23e0764cf61ee6c81e
ChrisM709 <christopher.meadus@keyin.com>
2025-06-16 08:57:00 -0300

    Added Task Class - created a constructor that takes a description and initializes completed to false for later changing if a task is done or not - added markCompleted() to update the task status once done, plus getters for description and isCompleted() - overrode toString() so printing a task shows [ ] Description or [x] Description based on its state

--- src/main/java/com/todolistmanager/Task.java
+++ src/main/java/com/todolistmanager/Task.java
@@ -1,5 +1,28 @@
 package com.todolistmanager;
 
 public class Task {
-    
+    private String description;  
+    private boolean completed;   
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[x] " : "[ ] ") + description;
+    }
 }
