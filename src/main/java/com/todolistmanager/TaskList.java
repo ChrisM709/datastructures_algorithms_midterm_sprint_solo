9b861edebb3b06bd1261a5ac57a7e7efbd424643
ChrisM709 <christopher.meadus@keyin.com>
2025-06-15 22:20:29 -0300

    Added TaskList.java - created this first because a user needs to add tasks to a list, next will be tasks being created - made sure the head pointed to the first node and size keeps track of the array size - created addTask(), new task is wrapped in a new node and if its empty it becomes the head if not its traversed to the end and linked to a new node there, - left markCompleted and PrintTaasks empty to imppliment other features later

--- src/main/java/com/todolistmanager/TaskList.java
+++ src/main/java/com/todolistmanager/TaskList.java
@@ -1,5 +1,42 @@
 package com.todolistmanager;
 
 public class TaskList {
-    
+    private static class Node {
+        Task task;
+        Node next; 
+
+        Node(Task task) {
+            this.task = task;
+        }
+    }
+
+    private Node head; 
+    private int size; 
+
+    public void addTask(Task newTask) {
+        Node node = new Node(newTask); 
+        if (head == null) {
+            head = node; 
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = node;
+        }
+        size++;
+    }
+
+    public void markCompleted(int position) {
+        // TODO: start at head, move `position` times, then call task.markCompleted()
+    }
+
+    public void printTasks() {
+        // TODO: start at head, loop through each node, and do
+        // System.out.println(node.task)
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
